e7e648af8745195f2d2b8b1dd0d76639dfd643a5
nitesh <niteshmeena9672@gmail.com>
2024-05-31 23:34:34 +0530

    anagram completed

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let ans = true;
+  // Convert strings to arrays for comparison
+  let arr1 = str1.split('');
+  let arr2 = str2.split('');
+  
+  // Check if lengths are different
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  // Sort arrays to compare them easily
+  arr1.sort();
+  arr2.sort();
+  
+  // Compare sorted arrays
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      ans = false;
+      break;
+    }
+    
+  }
+  
+  return ans;
 }
-
 module.exports = isAnagram;
